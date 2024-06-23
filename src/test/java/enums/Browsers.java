1fc1eb1d1f95d94381941fbbc8a3e26d758b0960
Julio Cesar <juliosantosjob>
2024-06-23 17:12:51 -0300

    Settings: Add options enums

---
+++ src/test/java/enums/Browsers.java
@@ -0,0 +1,39 @@
+package enums;
+
+public enum Browsers {
+    CHROME("webdriver.chrome.driver", "drivers/chromedriver.exe"),
+    CHROME_HEADLESS("webdriver.chrome.driver", "drivers/chromedriver.exe"),
+    FIREFOX("webdriver.gecko.driver", "drivers/geckodriver.exe"),
+    EDGE("webdriver.edge.driver", "drivers/msedgedriver.exe"),
+    EDGE_HEADLESS("webdriver.edge.driver", "drivers/msedgedriver.exe");
+
+    private final String property;
+    private final String path;
+
+    Browsers(String property, String path) {
+        this.property = property;
+        this.path = path;
+    }
+
+    public String getProperty() {
+        return property;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public static Browsers getBrowsers(String browser) {
+        return Browsers.valueOf(browser.toUpperCase());
+    }
+
+    public static String getPropertyDriver(String propertyDrowser) {
+        return Browsers.getBrowsers(propertyDrowser)
+                .getProperty().toString();
+    }
+
+    public static String getPathDriver(String pathDrowser) {
+        return Browsers.getBrowsers(pathDrowser)
+                .getPath().toString();
+    }
+}
--- src/test/java/support/DriverDefinition.java
+++ src/test/java/support/DriverDefinition.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 
+import enums.Browsers;
+
 import static java.lang.System.getProperty;
 
 public class DriverDefinition {
@@ -13,34 +15,43 @@ public class DriverDefinition {
     private static String browser = getProperty("BROWSER");
 
     public static void browserConfig() {
-        if (browser == null)
-            browser = "chrome";
+        if (browser == null) {
+            browser = "CHROME_HEADLES";
+        }
 
         switch (browser) {
-            case "chrome":
-                System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
-                ChromeOptions options = new ChromeOptions();
+            case "CHROME":
+                System.setProperty(Browsers.getPropertyDriver(browser),
+                        Browsers.getPathDriver(browser));
 
+                ChromeOptions options = new ChromeOptions();
                 options.addArguments("--remote-allow-origins=*");
+
                 driver = new ChromeDriver(options);
                 break;
 
-            case "edge":
-                System.setProperty("webdriver.edge.driver", "src/test/resources/drivers/msedgedriver.exe");
+            case "EDGE":
+                System.setProperty(Browsers.getPropertyDriver(browser),
+                        Browsers.getPathDriver(browser));
+
                 driver = new EdgeDriver();
                 break;
 
-            case "chrome-headless":
-                System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
+            case "CHROME_HEADLESS":
+                System.setProperty(Browsers.getPropertyDriver(browser),
+                        Browsers.getPathDriver(browser));
+
                 ChromeOptions optChrome = new ChromeOptions();
-                optChrome.addArguments("--headless");
+                optChrome.addArguments("-headless");
                 optChrome.addArguments("--remote-allow-origins=*");
                 optChrome.addArguments("--window-size=1920x1080");
                 driver = new ChromeDriver(optChrome);
                 break;
 
-            case "edge-headless":
-                System.setProperty("webdriver.edge.driver", "src/test/resources/drivers/msedgedriver.exe");
+            case "EDGE_HEADLESS":
+                System.setProperty(Browsers.getPropertyDriver(browser),
+                        Browsers.getPathDriver(browser));
+
                 EdgeOptions optEdge = new EdgeOptions();
                 optEdge.addArguments("headless");
                 optEdge.addArguments("--window-size=1920x1080");
