9c33524c088e7645244c46bfcd10ea0b2d6a15a8
Julio Santos <julioc.9730@gmail.com>
2024-08-22 17:24:00 -0300

    Update Envs.java

--- src/test/java/env/setup/Envs.java
+++ src/test/java/env/setup/Envs.java
@@ -5,12 +5,14 @@ import java.util.Properties;
 
 public class Envs {
     private static String PATH_PROJECT = System.getProperty("user.dir");
+    private static String PATH_PROP = "/src/test/java/env/resources/env.properties";
+    private static String LOAD_PATH = PATH_PROJECT + PATH_PROP;
     private static Properties prop;
 
     private static Properties loadProp() {
         try {
             Envs.prop = new Properties();
-            Envs.prop.load(new FileInputStream(PATH_PROJECT + "/src/test/java/env/resources/env.properties"));
+            Envs.prop.load(new FileInputStream(LOAD_PATH));
 
         } catch (Exception e) {
             System.out.println("**** WARNING: Missing env.properties. Add base URL! **** " + e);
