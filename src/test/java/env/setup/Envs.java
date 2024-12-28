9449fb998cca70aa2da7121c233ce99aa66e22c5
Julio Santos <julio958214@gmail.com>
2024-12-28 13:10:02 -0300

    Atualizar o Envs.java

--- src/test/java/env/setup/Envs.java
+++ src/test/java/env/setup/Envs.java
@@ -6,21 +6,18 @@ import java.util.Properties;
 public class Envs {
     private static String PATH_PROP = "/src/test/java/env/resources/env.properties";
     private static String LOAD_PATH = System.getProperty("user.dir") + PATH_PROP;
-    private static Properties prop;
+    private static Properties prop = new Properties();
 
     private static Properties loadProp() {
         try {
-            Envs.prop = new Properties();
-            Envs.prop.load(new FileInputStream(LOAD_PATH));
-
+            prop.load(new FileInputStream(LOAD_PATH));
         } catch (Exception e) {
             System.out.println("**** WARNING: Missing env.properties. Create the file and add base URL on key \"HMG\"! **** " + e);
         }
-        return Envs.prop;
+        return prop;
     }
 
     public static String getEnv(String env) {
-        return Envs.loadProp().getProperty(env);
+        return loadProp().getProperty(env);
     }
-    
 }
