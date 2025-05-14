261710d7a00d1f9255524568f0c42858a31898bf
Julio Santos <julio958214@gmail.com>
2025-05-14 17:28:42 -0300

    Update Screenshots.java

--- src/test/java/utils/Screenshots.java
+++ src/test/java/utils/Screenshots.java
@@ -10,40 +10,26 @@ import support.BasePage;
 import java.io.File;
 import java.io.IOException;
 
-import static java.lang.System.out;
-
 public class Screenshots extends BasePage {
-
-    /**
-     * Screenshots settings in the project using adaptation of the allure report.
-     */
 
     public static void shot(Scenario scenario) {
         try {
-            File screenshotAs = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+            File screenshotAs = ((TakesScreenshot) getDriver()).getScreenshotAs(OutputType.FILE);
             Allure.addAttachment("Screenshot", FileUtils.openInputStream(screenshotAs));
         } catch (RuntimeException | IOException e) {
-            throw new RuntimeException("Error implementing screenshot!");
+            throw new RuntimeException("Erro ao implementar captura de tela!");
         }
     }
-
-    /**
-     * Conditional for success or failure screenshots.
-     * in this method the screenshots will be executed at the end of each step, the
-     * necessary parameter for the execution would be the scenario itself to be
-     * executed.
-     */
 
     public static void takingScreenshot(Scenario scenario) {
         try {
             if (scenario.isFailed()) {
-                out.println("Scenario Failed: " + scenario.getName());
-            } else {
-                stop(1000);
+                System.out.println("Cenário falhou: " + scenario.getName());
             }
+            stop(1);
             shot(scenario);
-        } catch (Exception e) {
-            out.println("Erro ao capturar screenshot: " + e.getMessage());
+        } catch (RuntimeException e) {
+            throw new RuntimeException("Erro ao capturar screenshot", e);
         }
     }
 }
